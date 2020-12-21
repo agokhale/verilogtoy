adc3ed48bb417a9d729773968de622584654967c
agokhale <ash@aeria.net>
2020-12-21 18:05:16 -0500

    counting under inspection

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ vvp helo
 iverilog -o obj/ctr  ctr.vl
 iverilog -o obj/async_ctr  async_ctr.vl
 iverilog -o obj/clockbench clockbench.vl
-vvp -v obj/clockbench
+iverilog -o obj/clockish clockish.vl
+iverilog -o obj/blend clockish.vl clockbench.vl
+
+iverilog -o obj/tbctr  clockbench.vl ctr.vl tb_ctr.vl
+#vvp -v obj/clockbench
+#vvp -v obj/clockish
+#vvp -v obj/blend
+vvp -v obj/tbctr

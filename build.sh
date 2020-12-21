da6f29b5e28a5b5f3b2aa793e2f9c4d44d1b19ce
agokhale <ash@aeria.net>
2020-12-21 13:21:39 -0500

    works

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 mkdir -p obj
 rm -f *.vcd obj/*
-iverilog -o obj/clockbench clockbench.vl
 iverilog -o obj/helo helo.vl
+vvp helo
 iverilog -o obj/ctr  ctr.vl
 iverilog -o obj/async_ctr  async_ctr.vl
-vvp helo
+iverilog -o obj/clockbench clockbench.vl
 vvp -v obj/clockbench

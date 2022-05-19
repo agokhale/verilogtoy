2616133e115fa84c5447aed9adfc6b9eb195c071
ash <ash@aeria.net>
2022-05-19 16:01:56 -0400

    add pwm, clocksplit, gtkview templ

--- waves/build.sh
+++ waves/build.sh
@@ -3,5 +3,6 @@
 mkdir -p obj
 trg=obj/sw.vv
 
-iverilog -Wtimescale -o $trg sawtooth.vl ../clockish.vl tb_saw.vl
+iverilog -Wtimescale -o $trg sawtooth.vl ../clockish.vl  \
+	 tb_saw.vl pwm.vl clocksplitter.vl
 vvp -v $trg

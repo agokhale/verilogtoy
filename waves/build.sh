b30b137c807132daac6696fe5e1355a6c4abf38b
ash <ash@aeria.net>
2022-05-24 15:46:38 -0400

    add triangle wave

--- waves/build.sh
+++ waves/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -ex
 
 
-srcs="hbridge.vl sawtooth.vl ../clockish.vl pwm.vl"
+srcs="hbridge.vl sawtooth.vl ../clockish.vl pwm.vl triangle.vl clocksplitter.vl "
 mkdir -p obj
 trg=obj/sw.vv
 
 
 verilator --lint-only $srcs  || true
-iverilog -Wtimescale -o $trg sawtooth.vl ../clockish.vl  \
-	 tb_saw.vl pwm.vl clocksplitter.vl \
-		hbridge.vl
+iverilog -Wtimescale -o $trg  \
+	 tb_saw.vl  \
+		${srcs}
 vvp -v $trg

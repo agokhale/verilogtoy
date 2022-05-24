5710ea90db6cf8df9041f745cba3f00c7172ab75
ash <ash@aeria.net>
2022-05-24 12:19:00 -0400

    add hbbridge dead time proctector

--- waves/build.sh
+++ waves/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -ex
 
+
+srcs="hbridge.vl sawtooth.vl ../clockish.vl pwm.vl"
 mkdir -p obj
 trg=obj/sw.vv
 
+
+verilator --lint-only $srcs  || true
 iverilog -Wtimescale -o $trg sawtooth.vl ../clockish.vl  \
-	 tb_saw.vl pwm.vl clocksplitter.vl
+	 tb_saw.vl pwm.vl clocksplitter.vl \
+		hbridge.vl
 vvp -v $trg
